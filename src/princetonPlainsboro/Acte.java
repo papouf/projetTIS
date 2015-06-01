f75c38a250949c838c0034c2e5067fe3a61957e9
kermode <romain.jego@gmail.com>
2015-06-01 12:01:37 +0200

    ecriturexml

--- src/princetonPlainsboro/Acte.java
+++ src/princetonPlainsboro/Acte.java
@@ -3,6 +3,7 @@ package princetonPlainsboro;
 public class Acte {
     private Code code;
     private int coef;
+    private int de;
     
     public Acte(Code code, int coef) {
         this.code = code;
