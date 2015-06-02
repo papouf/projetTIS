c65bdbdc53b5ba19ac87ea8cf59d4393af0b7d5f
U-ST\Francois JEAN-MARIE <Francois JEAN-MARIE@GNB404612.gnb.st.com>
2015-06-02 09:40:13 +0200

    force recommit

--- src/interfaceH/ajoutFiche.java
+++ src/interfaceH/ajoutFiche.java
@@ -9,12 +9,12 @@ package interfaceH;
  *
  * @author margot
  */
-public class ajoutFiche extends javax.swing.JFrame {
+public class AjoutFiche extends javax.swing.JFrame {
 
     /**
      * Creates new form ajoutFiche
      */
-    public ajoutFiche() {
+    public AjoutFiche() {
         initComponents();
     }
 
@@ -181,20 +181,21 @@ public class ajoutFiche extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ajoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AjoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ajoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AjoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ajoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AjoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ajoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AjoutFiche.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ajoutFiche().setVisible(true);
+                new AjoutFiche().setVisible(true);
             }
         });
     }
--- src/interfaceH/log.java
+++ src/interfaceH/log.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author Julien
  */
-public class log extends javax.swing.JFrame {
+public class Log extends javax.swing.JFrame {
 
     /**
      * Creates new form log
      */
-    public log() {
+    public Log() {
         initComponents();
     }
 
@@ -183,20 +183,21 @@ public class log extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Log.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new log().setVisible(true);
+                new Log().setVisible(true);
             }
         });
     }
